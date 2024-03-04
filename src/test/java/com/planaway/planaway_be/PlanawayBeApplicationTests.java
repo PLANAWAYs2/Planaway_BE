4baa171dc7d6a11e0dbfd68746ba20200d24f847
밍주 <49716188+Minju-Kimm@users.noreply.github.com>
2024-03-04 21:49:16 +0900

    **SQL 연결, 회원가입, 로그인-> 데베 연결
    새벽까지 더 보완예정입니닷~

---
+++ src/test/java/com/planaway/planaway_be/PlanawayBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planaway.planaway_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanawayBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
